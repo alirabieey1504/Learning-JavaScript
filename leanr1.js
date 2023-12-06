21b57e7c3128c38c2b9b99d8318a5e5eea5b6816
alirabieey <139227807+alirabieey1504@users.noreply.github.com>
2023-12-06 21:34:27 +0330

    Add files via upload

--- leanr1.js
+++ leanr1.js
@@ -1,17 +1,6 @@
-var myage = 25;
+// Part 7
 
-alert(typeof myage)
-
-// name me
- 
-var name2 = "alirabiee"
-alert(typeof name2)
-
-var fact = true
-alert(typeof fact)
-var ismale = false
-alert(typeof ismale)
-
-/* get type of me age*/
-var me=12
-alert(me)
+var number = 12;
+//get name & family
+var userinfo = prompt("pleas name ","alirabieey")
+alert (userinfo)
