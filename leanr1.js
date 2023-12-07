245edf58ea8613961c5b878fe13d6c6b923adb84
alirabieey <139227807+alirabieey1504@users.noreply.github.com>
2023-12-07 14:11:00 +0330

    Add files via upload

--- leanr1.js
+++ leanr1.js
@@ -1,8 +1,11 @@
 // //part 8
-var n="2"
-var s="4"
-var a=s/n
-alert(typeof a)
-var f=Boolean(" ")
-alert(f) 
-// alert(typeof Number("123z"))
+var n=12
+var s=14
+var p=n+s
+var m=s-n
+var g=3
+var a=m/g
+var w=a*g
+var t=w**s
+var d=18%5
+alert(d)
