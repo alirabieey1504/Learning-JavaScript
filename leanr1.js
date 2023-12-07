e2b7ec991bce0777e1c006fbdc29bf6fd1cc2056
alirabieey <139227807+alirabieey1504@users.noreply.github.com>
2023-12-07 14:20:23 +0330

    Add files via upload

--- leanr1.js
+++ leanr1.js
@@ -1,11 +1,13 @@
-// //part 8
-var n=12
-var s=14
-var p=n+s
-var m=s-n
-var g=3
-var a=m/g
-var w=a*g
-var t=w**s
-var d=18%5
-alert(d)
+// //part 9
+var number1=prompt("please enter one number")
+var number2=prompt("please enter two number")
+var number1=Number(number1)
+var number2=Number(number2) 
+var jam=number1+number2
+alert(jam)
+var menha=number1-number2
+alert(menha)
+var zarb=number1*number2
+alert(zarb)
+var taghsim=number1/number2
+alert(taghsim)
