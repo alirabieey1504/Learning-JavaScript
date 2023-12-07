6321b2e8e146f192b4575847d2b6365c22278db6
alirabieey <139227807+alirabieey1504@users.noreply.github.com>
2023-12-07 12:44:57 +0330

    Add files via upload

--- leanr1.js
+++ leanr1.js
@@ -1,18 +1,10 @@
-// Part 7
-
-var number = 12;
-//result = prompt(title, [default]);
-let userinfo = prompt("pleas name ","alirabieey")
-alert ('My name is ali');
- 
-
-// part 8
-//type casting
-var username=prompt("pleas get age")
-var age= Number(username)
-
-alert(typeof age)
-//type casting short cut
-var std=prompt("pleas std")
-var sss= +std
-alert(typeof sss)
+// //part 8
+var n=prompt("pleas age")
+ var n=String(n)
+alert(typeof n)
+var bool=true
+var bool =String(bool)
+alert(typeof bool)
+var use=19
+var use=Boolean(use) 
+alert(typeof use)
