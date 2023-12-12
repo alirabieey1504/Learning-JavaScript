06917a00e03bb7409d32a71769ff3e7075c42cfe
alirabieey <139227807+alirabieey1504@users.noreply.github.com>
2023-12-12 19:49:20 +0330

    Add files via upload

---
+++ learn12.js
@@ -0,0 +1,23 @@
+var namesang = prompt("lotfan vaghti name sang ro goftam rang ra begoo")
+switch (namesang) {
+    case "space":
+        alert("rang an \"blue\" hast")
+        break;
+    case "mind":
+        alert("rang an \"yello\" hast")
+        break;
+    case "reality":
+        alert("rang an \"red\" hast")
+        break;
+    case "power":
+        alert("rang an \"purple\" hast")
+        break;
+    case "time":
+        alert("rang an \"green\" hast")
+        break;
+    case "soul":
+        alert("rang an \"orange\" hast")
+        break;
+        default:alert("sang yaft nashod")
+
+}
