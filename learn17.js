8346cf8669006ac4c7f51a85f8241508bc442d64
alirabieey <139227807+alirabieey1504@users.noreply.github.com>
2023-12-12 23:00:37 +0330

    Add files via upload

---
+++ learn17.js
@@ -0,0 +1,23 @@
+// if 
+
+var num1 = 12
+var num2 = 3
+var num3 = 4
+
+if (num2 * num3 == num2) {
+    // code
+
+    alert('yes')
+}
+ else if(num2 / num1 == num3)
+{
+    alert("no")
+}
+else {
+    alert("yaft na shod")
+}
+let age = prompt('age?', 18);
+let message = (age < 3) ? alert("hi baby") :
+  (age < 18) ? alert("hello") :
+  (age < 100) ? alert('Greetings!') :
+   alert('What an unusual age!');
