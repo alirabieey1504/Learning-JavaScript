7e95da8d2ed79b8a44be970674ffc5336e73ca3f
alirabieey <139227807+alirabieey1504@users.noreply.github.com>
2023-12-12 20:10:01 +0330

    Add files via upload

---
+++ learn13.js
@@ -0,0 +1,12 @@
+var str1= "sabzlearn.ir "
+
+var num1='12'
+var num2=4
+var s=str1*num1
+alert(num2 - num1)
+
+alert(typeof s)
+// NAN => nat a number : /, * ,-, %
+// concat +
+//type of nan=>number
+alert(4+ "12") // 124
