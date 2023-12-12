aa08a17525ead3daf5d580301eda528f09a3e797
alirabieey <139227807+alirabieey1504@users.noreply.github.com>
2023-12-12 22:11:28 +0330

    Add files via upload

---
+++ learn16.js
@@ -0,0 +1,13 @@
+var num1=12
+var num2=3
+var num3=4
+
+var str2="3"
+var str3="4"
+
+//alert(num3 * num2 ==num1)
+// alert(num1 / num2 ==num3)
+// alert(num1<num2)
+alert(num2==str2)
+
+alert(num2 ===str2)
