01680eb925105bce2d2efad675c9ee6639b32936
alirabieey <139227807+alirabieey1504@users.noreply.github.com>
2023-12-14 14:57:33 +0330

    Add files via upload

---
+++ learn23.js
@@ -0,0 +1,24 @@
+var userbirthdate = prompt("enter your birthday", 1370)
+
+var mohasebe = 1402 - userbirthdate
+
+if(isNaN(mohasebe)){
+    alert("اشتپ زدی داش")
+}
+else{
+    alert("birthday : " + mohasebe+"/")
+}
+
+
+
+
+
+
+
+
+
+
+// var data=new data()
+// var year=data.getFullYear()
+//  var totul=tarikh-year
+// alert(totul)
