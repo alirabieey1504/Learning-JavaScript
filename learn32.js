14eec9a88e6a4a03803297bdd744694902fca82d
alirabieey <139227807+alirabieey1504@users.noreply.github.com>
2023-12-18 21:10:20 +0330

    Add files via upload

---
+++ learn32.js
@@ -0,0 +1,13 @@
+// //function 
+// function showtext(usertext) {
+//     alert("me alert showtext :" + usertext)
+// }
+
+// showtext(3)
+function showtwonumbers(firstnumber,secondnumber){
+    alert("first parametr :"+firstnumber)
+    alert("second parametr :"+secondnumber)
+}
+var num1=prompt("enter to number1")
+var num2=prompt("enter number2")
+showtwonumbers(num1,num2)
