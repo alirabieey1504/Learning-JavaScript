dfd4d81ea668311f5d97ba38374239820961555b
alirabieey <139227807+alirabieey1504@users.noreply.github.com>
2023-12-18 16:47:13 +0330

    Add files via upload

---
+++ learn26.js
@@ -0,0 +1,25 @@
+var num2=4;
+var num3=3;
+
+if(num2==+2&& num2*num3===12)
+alert("")
+else if(num3===3 && num2*num3===12)
+alert("shart dovom doroste")
+else
+alert("hich kodom bargharar nist!")
+
+
+
+
+//or یا ||
+//AND , &&
+//true && true => true
+
+///////////////////////////////////////////////////////
+ var a=-3
+ alert(!a)
+    var boole1=true
+    alert("my boolean =" +boole1)
+    alert("!my boolean ="+!boole1)
+    alert("!!mybolean ="+!!boole1)
+
