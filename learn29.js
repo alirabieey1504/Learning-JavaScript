807cd35c209b66184a9a60c3bdc20e0cbf99c38b
alirabieey <139227807+alirabieey1504@users.noreply.github.com>
2023-12-18 19:24:15 +0330

    Add files via upload

---
+++ learn29.js
@@ -0,0 +1,35 @@
+var score = prompt("please enter math score :")
+
+switch (score) {
+    case "20":
+    case "19":
+    case "18":
+        alert("damet garm");
+        break;
+    case "17":
+    case "16":
+    case "15":
+        alert("b")
+        break;
+    case "14":
+    case "13":
+    case "12":
+        alert("c")
+        break;
+    case "12":
+    case "11":
+    case "10":
+    case "9":
+    case "8":
+    case "7":
+    case "6":
+    case "5":
+    case "4":
+    case "3":
+    case "2":
+    case "1":
+        alert("oftadi ke mashti")
+        break;
+    default:
+        alert("motmaeni ke doroste ?")
+}
---
+++ learn30.js
@@ -0,0 +1,13 @@
+function showValue() {
+    var num1 = 2
+    var num3 = 8
+    var result = num1 + num3
+    alert("result :" + result)
+    
+}
+
+showValue()
+showValue()
+showValue()
+showValue()
+showValue()
