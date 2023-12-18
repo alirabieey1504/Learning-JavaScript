1280cb769df5e18469ce4aa7b5fdeda0cc3fa5cd
alirabieey <139227807+alirabieey1504@users.noreply.github.com>
2023-12-18 20:28:38 +0330

    Add files via upload

---
+++ learn31.js
@@ -0,0 +1,15 @@
+//نحوه تعریف دیگر فانکشن ها 
+
+var sayhey=function(){
+    var num1=2
+    var num2=3
+    var result=num1 +num2;
+    alert("num1 + num2 =" + result)
+}
+
+var examp=function (){
+    alert("Hi")
+}
+
+sayhey()
+examp()
