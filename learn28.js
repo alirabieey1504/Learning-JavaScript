b5ca14ae6e06d4e85f81ae267acc1e39ee3a22d8
alirabieey <139227807+alirabieey1504@users.noreply.github.com>
2023-12-18 17:16:45 +0330

    Add files via upload

---
+++ learn28.js
@@ -0,0 +1,16 @@
+var name1 = 3;
+var num2 = 4;
+var num3 = name1 * num2;
+switch (num3) {
+    case 10: alert("maghdar barabar 10");
+        break;
+    case 11: alert("barabar 11");
+        break;
+    case 12: alert("barabar 12");
+        break;
+    case 13: alert("fanaf 13");
+        break;
+    default:
+        alert("hich kodam ");
+
+}
