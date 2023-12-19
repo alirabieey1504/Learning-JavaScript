9fc4d64859c86a1473ca45ae5f20002bb512846d
alirabieey <139227807+alirabieey1504@users.noreply.github.com>
2023-12-19 15:05:43 +0330

    Add files via upload

---
+++ learn34.js
@@ -0,0 +1,12 @@
+function avrage(nu1,nu2,nu3) {
+   
+    var totul = nu1 + nu2 + nu3;
+    var avr= totul/3
+     alert("avrage :"+avr)
+   
+}
+
+var num1 = +prompt("enter number 1 :")
+var num2 = +prompt("enter number 2 :")
+var num3 = +prompt("enter number 3 :")
+avrage(num1,num2,num3)
