af8334a937075be54995f6e97451b93cdd7c6d37
alirabieey <139227807+alirabieey1504@users.noreply.github.com>
2023-12-19 19:29:38 +0330

    Add files via upload

---
+++ learn39.js
@@ -0,0 +1,12 @@
+var name1= " aI love java script          "
+console.log(name1[2])
+console.log(name1.charAt(4))
+
+console.log(name1.charCodeAt(0))
+console.log(name1.concat(" & react.js"))
+console.log(name1.trim())
+console.log(name1.toLowerCase())
+console.log(name1.toUpperCase())
+console.log(name1.search("love"))
+console.log(name1.indexOf("love"))
+console.log(name1.includes("love"))
