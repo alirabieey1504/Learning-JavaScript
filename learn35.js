06ac5625859b3ed521d65d8d25881b996f586117
alirabieey <139227807+alirabieey1504@users.noreply.github.com>
2023-12-19 15:27:44 +0330

    Add files via upload

---
+++ learn35.js
@@ -0,0 +1,11 @@
+function sum(num1 = 4, num2 = 1234) {
+    // if (num1 == undefined)
+    //     num1 = 1
+    // if (num2 == undefined)
+    //     num2 = 52
+    var result = num1 + num2
+    alert("sum :" + result)
+}
+
+
+sum(23)          
