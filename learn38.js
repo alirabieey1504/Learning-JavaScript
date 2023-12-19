786bc388304e66e7f2ae31676080eb24ea3766fd
alirabieey <139227807+alirabieey1504@users.noreply.github.com>
2023-12-19 16:48:51 +0330

    Add files via upload

---
+++ learn38.js
@@ -0,0 +1,8 @@
+
+var username = prompt("please enter username :")
+var password = prompt("please enter password :")
+if (username.length >= 3 && password.length >= 8)
+    alert("daryaft shod")
+else
+    alert("khata username hadaghal 3ta va password ham 8ta")
+
