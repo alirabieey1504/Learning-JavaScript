21d28af3895689a0f319827aa3e2126028036ab7
alirabieey <139227807+alirabieey1504@users.noreply.github.com>
2023-12-19 15:59:03 +0330

    Add files via upload

---
+++ learn36.js
@@ -0,0 +1,15 @@
+// function sum(num1, num2) {
+//     var result = num1 + num2
+//    return result
+// }
+
+// var sumresult = sum(2, 3)
+// alert("sumresult :"+sumresult)
+
+function hover (num1,num2){
+    return num1**num2
+}
+var number1= prompt("enter first number")
+var number2= prompt("enter first number")
+var result=hover(number1,number2)
+alert("result :" +result)
