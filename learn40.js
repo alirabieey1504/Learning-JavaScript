9a86c5025de5b74b7bf6b7397f00cfecdb67dee8
alirabieey <139227807+alirabieey1504@users.noreply.github.com>
2023-12-19 19:29:51 +0330

    Add files via upload

---
+++ learn40.js
@@ -0,0 +1,8 @@
+var username = prompt("please enter username :")
+//حساس نبودن به حروف کوچک و برزرگ 
+if (username.toLowerCase() === "ali") {
+    alert("khoshamadi ali")
+}
+else {
+    alert("eshtebah vared shodi")
+}
