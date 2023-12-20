5f9c1b1633a86eee38b266f238989252732de83f
alirabieey <139227807+alirabieey1504@users.noreply.github.com>
2023-12-20 17:10:38 +0330

    Add files via upload

---
+++ learn43.js
@@ -0,0 +1,14 @@
+//assignment operator
+
+var num1=10
+// num1=num1+10
+num1+=1
+// num1=num1-4
+num1-=4
+// num1=num1*3
+num1 *=3
+// num1=num1/2
+num1/= 2
+// num1=num1**2
+num1**=2
+console.log(num1)
