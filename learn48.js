57e9ea7df5c2b9a10dda8cd9aba3144a9a917740
alirabieey <139227807+alirabieey1504@users.noreply.github.com>
2023-12-21 13:45:26 +0330

    Add files via upload

---
+++ learn48.js
@@ -0,0 +1,11 @@
+
+
+var temp = 0
+
+for (i = 1; i <=5; i++) {
+    var number = +prompt("adad " + i  + " vared konid  :")
+
+    temp += number;
+}
+var avrage = temp / 5
+alert("avrage :" + avrage)
