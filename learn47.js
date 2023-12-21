510c82b66b70fede18c1dc7c79200e4dd61d0d20
alirabieey <139227807+alirabieey1504@users.noreply.github.com>
2023-12-21 12:03:09 +0330

    Add files via upload

---
+++ learn47.js
@@ -0,0 +1,9 @@
+var num = +prompt("enter number mahsol")
+var temp = 0;
+for (i = 0; i < num; i++) {
+
+    var gheymat = +prompt("lotfan gheymat  ra vared konin :")
+    temp += gheymat
+
+}
+alert("gheymat nahee :"+temp)
