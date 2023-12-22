9f8e7e32d90e0e5eaf0c569fdaf434ee825ead62
alirabieey <139227807+alirabieey1504@users.noreply.github.com>
2023-12-22 17:08:03 +0330

    Add files via upload

---
+++ learn54.js
@@ -0,0 +1,13 @@
+
+var sum=0;
+var i;
+for(i=0;;i++){
+     var numb=+prompt("adad ra vared kon :")
+     sum+=numb
+     if(numb==-1){
+        break;
+     }
+}
+
+var avrage=sum/i
+alert("avrage :"+avrage)
