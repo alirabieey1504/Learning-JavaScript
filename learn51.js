136e749bba13e326f2dd85f5fca104435a0aeba3
alirabieey <139227807+alirabieey1504@users.noreply.github.com>
2023-12-22 14:36:38 +0330

    Add files via upload

---
+++ learn51.js
@@ -0,0 +1,19 @@
+// var i = 0;
+// while (i < 100) {
+//     console.log(i)
+//     alert(i)
+//     i++;
+//     i++;
+// }
+
+var number=+prompt("enter chand number :")
+var i=0;
+var sum=0;
+while(i<number){
+    var cin=+prompt("enter number  :")
+    i++;
+    sum +=cin
+    
+}
+var avrage=sum/number;
+alert('avrage :'+avrage)
