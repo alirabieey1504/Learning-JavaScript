01b379afd71d1c6377a75b0bfeaf27505f659fcf
alirabieey <139227807+alirabieey1504@users.noreply.github.com>
2023-12-23 14:31:49 +0330

    Add files via upload

---
+++ learn56.js
@@ -0,0 +1,5 @@
+var arr=new Array();
+var lops=['for','while','dowhile'];
+var studens=['ali','ehsan','saman','sasan','farshad','sajad',]
+// console.log(studens.length);
+// console.log(studens[0])
