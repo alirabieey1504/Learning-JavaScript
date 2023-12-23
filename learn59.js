9672d95d551535c03d1a5f33191110db602c5429
alirabieey <139227807+alirabieey1504@users.noreply.github.com>
2023-12-23 16:28:59 +0330

    Add files via upload

---
+++ learn59.js
@@ -0,0 +1,23 @@
+// var students = ['ali', 'ahmad', 'arash', 'sobhan']
+// var student = {
+//     name: 'amin',
+//     family: 'rabiee',
+//     age: 32,
+//     phone: 8329,
+//     id:312
+// };
+// // console.log(student)
+// console.log(student['name'])
+
+
+var users = [
+    {
+        id: 1,
+        name: "ali",
+        family: "rabiee",
+        age: 21
+    },
+    { id: 2, name: "hosein", family: "rahmani", age: 23 }
+]
+
+console.log(users[1].age)
