1e59c6917bc850de2ea46df5ac8613fb575da8c1
alirabieey <139227807+alirabieey1504@users.noreply.github.com>
2023-12-23 15:09:27 +0330

    Add files via upload

---
+++ learn57.js
@@ -0,0 +1,20 @@
+// var score=[22,221,12,43,44,62,24]
+// console.log(score)
+// score[7]=22;
+// score[8]=13;
+// score.push(3,16,20,2,13,4,15);
+// score.pop()
+// console.log(score)
+
+
+/////////////////////////////////////////////
+
+var score=[22,221,12,43,44,62,24]
+score.shift()
+score.shift()
+score.shift()
+score.shift()
+score.unshift(10,12,13,14)
+score[3]=11
+
+console.log(score)
