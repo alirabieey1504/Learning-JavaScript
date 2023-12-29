fc24a4e40bc050ad177a04e84ce3c3bb6f644433
alirabieey <139227807+alirabieey1504@users.noreply.github.com>
2023-12-29 14:31:52 +0330

    Add files via upload

---
+++ learn81.js
@@ -0,0 +1,13 @@
+var chek=prompt("enter true or false")
+
+var timer=setTimeout(function(){
+    if(chek){
+        alert("yse")  
+    }
+    else{
+        alert("no")
+    }
+},5000)
+
+console.log(timer)
+ 
